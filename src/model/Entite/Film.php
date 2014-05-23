087c3a75329985a4f5f5642eeafa92ea898d3dfb
Karim LAMOURI <karimlamouri@me.com>
2014-05-23 14:09:36 +0200

    Ajout des getter/setter pour id

--- src/model/Entite/Film.php
+++ src/model/Entite/Film.php
@@ -27,6 +27,14 @@ class Film
 
         return $this;
     }
+    
+    public function setId($id){
+    	$this->id = $id;
+    }
+    
+    public function getId(){
+    	return $this->id;
+    }
 
     public function setTitre($titre) {
         $this->titre = $titre;
