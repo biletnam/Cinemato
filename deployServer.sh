b3846088af3d2f279d316c87e7e4baf706007ee8
fschildk <florent.schildknecht@gmail.com>
2014-05-27 00:22:14 +0200

    Update local deployment script

--- deployServer.sh
+++ deployServer.sh
@@ -1,4 +1,8 @@
 #! /bin/sh
 
-git pull
-chmod -R 755 .
+ssh nf17p157@tuxa.sme.utc "\
+cd /volsme/user1x/uvs/nf17/nf17p157/public_html;\
+git pull origin master;
+chmod -R 755 ./;
+exit;";
+echo "Project has been deployed !";
