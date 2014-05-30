68bc697bd16e7f8fa702b91086362879b30e9578
fschildk <florent.schildknecht@gmail.com>
2014-05-30 09:22:18 +0200

    Update scripts

--- updateServer.sh
+++ updateServer.sh
@@ -5,6 +5,7 @@ cd /volsme/user1x/uvs/nf17/nf17p157/public_html;\
 git stash;\
 git checkout master;\
 git pull origin master;\
+php composer.phar self-update;\
 php composer.phar update --no-dev;\
 chmod -R 755 ./;\
 exit;";
